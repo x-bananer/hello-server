271de26b0c92aa8149c3dd3b3c02581eab54c772
Kseniia Shlenskaia <xeniya.shlyonskaya@gmail.com>
2026-08-25 17:08:56 +0300

    fix: remove fallback

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 
 dotenv.config({
-	path: `.env.${process.env.NODE_ENV || 'development'}`
+	path: `.env.${process.env.NODE_ENV}`
 });
 
 const app = express();
